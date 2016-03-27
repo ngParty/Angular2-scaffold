deb1afaded868e5758020dcd7b7e9c88e009aa15
elmariofredo <mario@vejlupek.cz>
2016-03-27 12:39:29 +0200

    docs( tsconfig ): add emitDecoratorMetadata explanation

--- src/client/vendor.ts
+++ src/client/vendor.ts
@@ -2,5 +2,5 @@
 // https://github.com/angular/angular/blob/85f3dc2fb5f90fef8798b77240a3a242b4d29f7e/gulpfile.js#L1377
 import 'angular2/bundles/angular2-polyfills';
 
-// Angular 2 Deps
+// Rx as Angular 2 required dependency
 import 'rxjs/Rx';
